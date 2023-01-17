6b3c02f8240c22d32e60acca09d944b9795468fa
김응관 <kwhn9486@naver.com>
2023-01-17 13:39:48 +0900

    [Add] TosView 추가 #9

---
+++ Hapit/Hapit/View/LogIn/Normal/ToSView.swift
@@ -0,0 +1,20 @@
+//
+//  ToSView.swift
+//  Hapit
+//
+//  Created by 김응관 on 2023/01/17.
+//
+
+import SwiftUI
+
+struct ToSView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToSView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToSView()
+    }
+}

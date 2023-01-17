16198d7392ad698d03de8ddc44bf9c51d9d10793
Choo Hyun Ho <hyunho4178@gmail.com>
2023-01-17 10:45:43 +0900

    [Add] add HabitDetailView (#2)
    [Add] add HabitDetailView

---
+++ Hapit/Hapit/View/HabitDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  HabitDetailView.swift
+//  Hapit
+//
+//  Created by 추현호 on 2023/01/17.
+//
+
+import SwiftUI
+
+struct HabitDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HabitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        HabitDetailView()
+    }
+}

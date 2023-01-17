da459c349f56265e0de2b24195085bb8951d0622
김응관 <kwhn9486@naver.com>
2023-01-18 01:18:16 +0900

    [Add] GetStartView (회원가입 완료 후 뷰) 추가 #17

---
+++ Hapit/Hapit/View/LogIn/Normal/SignUp/GetStartView.swift
@@ -0,0 +1,20 @@
+//
+//  GetStartView.swift
+//  Hapit
+//
+//  Created by 김응관 on 2023/01/18.
+//
+
+import SwiftUI
+
+struct GetStartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GetStartView_Previews: PreviewProvider {
+    static var previews: some View {
+        GetStartView()
+    }
+}

07b06b7592e0832856dfe8b4e174eaeb644c22c3
김응관 <kwhn9486@naver.com>
2023-01-17 16:14:34 +0900

    [Move] SingUpView -> RegisterView로 이름 변경 & RegisterView, ToSView를 SignUp 폴더로 옮김 #17

---
+++ Hapit/Hapit/View/LogIn/Normal/SignUpTab.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpTab.swift
+//  Hapit
+//
+//  Created by 김응관 on 2023/01/17.
+//
+
+import SwiftUI
+
+struct SignUpTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpTab_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpTab()
+    }
+}

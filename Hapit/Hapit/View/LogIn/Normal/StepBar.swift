b51c7cd3087c8a6f32bd55670e51c7a1948e42a8
김응관 <kwhn9486@naver.com>
2023-01-18 03:29:10 +0900

    [Add] 회원가인 단계 Indicator 제작 완료 #17

--- Hapit/Hapit/View/LogIn/Normal/StepBar.swift
+++ Hapit/Hapit/View/LogIn/Normal/StepBar.swift
@@ -8,13 +8,69 @@
 import SwiftUI
 
 struct StepBar: View {
+    let nowStep: Int
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            ForEach(1...3, id: \.self) { index in
+                
+                if index <= nowStep {
+                    Spacer().frame(width: 8)
+                    Circle()
+                        .fill(Color.pink)
+                        .frame(width: 40)
+                        .overlay {
+                            Text("\(index)")
+                                .font(.title2)
+                                .foregroundColor(Color.white)
+                        }
+                } else {
+                    Spacer().frame(width: 8)
+                    Circle()
+                        .stroke(Color.gray, lineWidth: 0.7)
+                        .frame(width: 40)
+                        .overlay {
+                            Text("\(index)")
+                                .font(.title2)
+                                .foregroundColor(index == nowStep ? Color.white : Color.gray)
+                        }
+                }
+                
+                Spacer().frame(width: 8)
+                
+                if index != 3 {
+                    if index < nowStep {
+                        HStack {
+                            Circle()
+                                .fill(Color.pink)
+                                .frame(width: 3)
+                            Circle()
+                                .fill(Color.pink)
+                                .frame(width: 3)
+                            Circle()
+                                .fill(Color.pink)
+                                .frame(width: 3)
+                        }
+                    } else {
+                        HStack {
+                            Circle()
+                                .fill(Color.gray)
+                                .frame(width: 3)
+                            Circle()
+                                .fill(Color.gray)
+                                .frame(width: 3)
+                            Circle()
+                                .fill(Color.gray)
+                                .frame(width: 3)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 struct StepBar_Previews: PreviewProvider {
     static var previews: some View {
-        StepBar()
+        StepBar(nowStep: 2)
     }
 }

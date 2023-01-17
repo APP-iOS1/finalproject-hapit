4d5b9113a5ce936e53e553f8c48d6bf370c79077
yewom5858 <nerino5885@gmail.com>
2023-01-17 16:30:23 +0900

    [UI] 화면 텝 구성 설정 완료

---
+++ Hapit/Hapit/View/SocialTab/SocialView.swift
@@ -0,0 +1,20 @@
+//
+//  SocialView.swift
+//  Hapit
+//
+//  Created by 김예원 on 2023/01/17.
+//
+
+import SwiftUI
+
+struct SocialView: View {
+    var body: some View {
+        Text("소셜")
+    }
+}
+
+struct SocialView_Previews: PreviewProvider {
+    static var previews: some View {
+        SocialView()
+    }
+}

4a15a41b7a5fc6a4245ae874a73c3b00abb50e67
zoohee <jjhh1545@gmail.com>
2023-01-18 09:47:36 +0900

    [Feat] Add Profile Photo Sheet #14

--- Hapit/Hapit/View/MyPageTab/Profile/ProfileModalView.swift
+++ Hapit/Hapit/View/MyPageTab/Profile/ProfileModalView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct ProfileModalView: View {
+    @Binding var showModal: Bool
+    
+    let data = Array(1...40).map { "목록 \($0)"}
+    
+    //화면을 그리드형식으로 꽉채워줌
+    let columns = [
+        GridItem(.adaptive(minimum: 100),spacing: 5),
+        GridItem(.adaptive(minimum: 100),spacing: 5),
+        GridItem(.adaptive(minimum: 100),spacing: 5),
+        GridItem(.adaptive(minimum: 100),spacing: 5),
+        GridItem(.adaptive(minimum: 100),spacing: 5)
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            VStack{
+                LazyVGrid(columns: columns, spacing: 10) {
+                    ForEach(data, id: \.self) {i in
+                        Image("bearYellow")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .padding(5)
+                            .overlay(RoundedRectangle(cornerRadius: 20).stroke( Color.gray, lineWidth: 2))
+                    }
+                }
+                
+            }.padding()
+        }
     }
 }
 
 struct ProfileModalView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileModalView()
+        ProfileModalView(showModal: .constant(false))
     }
 }

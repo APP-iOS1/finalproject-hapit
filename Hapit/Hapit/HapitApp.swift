23aa8e326aa530e20cdfc8359b0c0191fcea6d54
Choo Hyun Ho <hyunho4178@gmail.com>
2023-01-31 14:07:57 +0900

    [Add] OneSignal Integration
    [Add] OneSignal Integration

--- Hapit/Hapit/HapitApp.swift
+++ Hapit/Hapit/HapitApp.swift
@@ -7,14 +7,34 @@
 
 import SwiftUI
 import FirebaseCore
+import OneSignal
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+class AppDelegate: NSObject, UIApplicationDelegate, OSSubscriptionObserver {
+    func onOSSubscriptionChanged(_ stateChanges: OSSubscriptionStateChanges) {
+        
+    }
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        requestNotificationPermission()
+        OneSignal.add(self as OSSubscriptionObserver)
+        OneSignal.initWithLaunchOptions(launchOptions)
+        OneSignal.setAppId("d3ac2497-479d-4054-b6b1-e6dc03a64acd")
+        
+        return true
+    }
+    func requestNotificationPermission(){
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge], completionHandler: {didAllow,Error in
+            if didAllow {
+                print("Push: 권한 허용")
+            } else {
+                print("Push: 권한 거부")
+            }
+        })
+    }
+    
+    
 }
 
 @main

9187ba70722fce78e6adbc4738caf80ab829c9e0
JINHYUNGP <37439745+JINHYUNGP@users.noreply.github.com>
2023-02-01 16:30:39 +0900

    [Feat] 커스텀 캘린더에서 사용될 커스텀 모달 뷰 제작#2
    커스텀 캘린더에서 날짜를 클릭했을 때 팝업 뷰 처럼 보이면서 드래그 제스쳐로 dismiss되는 모달 뷰가 띄워지도록 했습니다.

---
+++ Hapit/Hapit/View/HomeTab/HabitDetail/CustomModal/ModalView.swift
@@ -0,0 +1,119 @@
+//
+//  ModalView.swift
+//  Hapit
+//
+//  Created by 박진형 on 2023/02/01.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    
+    // Modal State
+    @Binding var modal: Modal
+    @GestureState var dragState: DragState = .inactive
+    
+    var animation: Animation {
+        Animation
+            .interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0)
+            .delay(0)
+    }
+    
+    var body: some View {
+        
+        let drag = DragGesture(minimumDistance: 30)
+            .updating($dragState) { drag, state, transaction in
+                state = .dragging(translation:  drag.translation)
+        }
+        .onChanged {
+            self.modal.dragOffset = $0.translation
+        }
+        .onEnded(onDragEnded)
+        
+        return GeometryReader(){ geometry in
+            ZStack(alignment: .top) {
+                Color.black
+                    .opacity(self.modal.position != .closed ? 0.3 : 0)
+                    .ignoresSafeArea()
+//                    .onTapGesture {
+//                        self.modal.position = .closed
+//                }
+                ZStack(alignment: .top) {
+                    Color("Default")
+                    self.modal.content
+                        .frame(height: UIScreen.main.bounds.height - (self.modal.position.offsetFromTop() + geometry.safeAreaInsets.top + self.dragState.translation.height))
+                }
+                .mask(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                .offset(y: max(0, self.modal.position.offsetFromTop() + self.dragState.translation.height + geometry.safeAreaInsets.top))
+                .gesture(drag)
+                .animation(self.dragState.isDragging ? nil : self.animation, value: modal.position)
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+    
+    private func onDragEnded(drag: DragGesture.Value) {
+        
+        // Setting stops
+        let higherStop: ModalState
+        let lowerStop: ModalState
+        
+        // Nearest position for drawer to snap to.
+        let nearestPosition: ModalState
+        
+        // Determining the direction of the drag gesture and its distance from the top
+        let dragDirection = drag.predictedEndLocation.y - drag.location.y
+        let offsetFromTopOfView = modal.position.offsetFromTop() + drag.translation.height
+        
+        // Determining whether drawer is above or below `.partiallyRevealed` threshold for snapping behavior.
+        if offsetFromTopOfView <= ModalState.partiallyRevealed.offsetFromTop() {
+            higherStop = .open
+            lowerStop = .partiallyRevealed
+        } else {
+            higherStop = .partiallyRevealed
+            lowerStop = .closed
+        }
+        
+        // Determining whether drawer is closest to top or bottom
+        if (offsetFromTopOfView - higherStop.offsetFromTop()) < (lowerStop.offsetFromTop() - offsetFromTopOfView) {
+            nearestPosition = higherStop
+        } else {
+            nearestPosition = lowerStop
+        }
+        
+        // Determining the drawer's position.
+        if dragDirection > 0 {
+            modal.position = lowerStop
+        } else if dragDirection < 0 {
+            modal.position = higherStop
+        } else {
+            modal.position = nearestPosition
+        }
+        
+    }
+    
+}
+
+enum DragState {
+    
+    case inactive
+    case dragging(translation: CGSize)
+    
+    var translation: CGSize {
+        switch self {
+        case .inactive:
+            return .zero
+        case .dragging(let translation):
+            return translation
+        }
+    }
+    
+    var isDragging: Bool {
+        switch self {
+        case .inactive:
+            return false
+        case .dragging:
+            return true
+        }
+    }
+}

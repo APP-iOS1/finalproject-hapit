22cf7a40df10e46b23eb3ce2aeb92369543d1e04
Choo Hyun Ho <hyunho4178@gmail.com>
2023-02-11 18:00:20 +0900

    [Fix] FCM function fixed (#123)
    [Fix] FCM function fixed (#123)

--- Hapit/Hapit/FCMManager.swift
+++ Hapit/Hapit/FCMManager.swift
@@ -26,49 +26,7 @@ final class FCMManager: ObservableObject {
             didChange.send(data)
         }
     }
-    
-    func sendMessageToDevice(){
-        guard let url = URL(string: "http://fcm.googleapis.com/fcm/send") else {
-            return
-        }
         
-        let json: [String: Any] = [
-            
-            "to": deviceToken,
-            "notification": [
-                
-                "title": titleText,
-                "body": bodyText
-            ],
-            "data": [
-            ]
-        ]
-    
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.httpBody = try? JSONSerialization.data(withJSONObject: json, options: [.prettyPrinted])
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("key=\(serverKey)", forHTTPHeaderField: "Authorization")
-        
-        let session = URLSession(configuration: .default)
-        
-        session.dataTask(with: request) { _, _, err in
-            if let err = err {
-                print(err.localizedDescription)
-                return
-            }
-            
-            print("Success")
-            DispatchQueue.main.async {[self] in
-                titleText = ""
-                bodyText = ""
-                deviceToken = ""
-            }
-        }
-        .resume()
-    }
-    
     func sendMessageTouser(datas:FCMManager, to token: String, title: String, body: String) {
         print("sendMessageTouser()")
         let urlString = "https://fcm.googleapis.com/fcm/send"

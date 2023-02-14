4bec8a690d3d6778564d8b867de94498dc687efd
김응관 <kwhn9486@naver.com>
2023-02-15 02:28:32 +0900

    [Fix] GetStartView iOS 15/16 버전 레이아웃 설정완료 #192

--- Hapit/Hapit/View/LogIn/Normal/SignUp/GetStartView.swift
+++ Hapit/Hapit/View/LogIn/Normal/SignUp/GetStartView.swift
@@ -15,49 +15,72 @@ struct GetStartView: View {
     @Binding var email: String
     @Binding var pw: String
     
-    var deviceWidth = UIScreen.main.bounds.size.width
     var deviceHeight = UIScreen.main.bounds.size.height
+    
+    var fontSize: CGFloat {
+        if deviceHeight < CGFloat(700.0) {
+            return 14
+        } else if deviceHeight >= CGFloat(700.0) && deviceHeight < CGFloat(820.0) {
+            return 15
+        } else if deviceHeight >= CGFloat(820.0) && deviceHeight < CGFloat(860.0) {
+            return 16
+        } else {
+            return 17
+        }
+    }
     
     // 화면비율 1/4 : 1/12.5 : 1/50
     var body: some View {
         GeometryReader { geo in
             VStack {
                 Group {
-                    // 2. iOS 버전을 기준으로 StepBar 분기처리
-                    // 3. 분기 내에서 디바이스 높이를 기준으로 GuideText 분기처리
+                    // 1. iOS 버전을 기준으로 StepBar 분기처리
+                    // 2. 분기 내에서 디바이스 높이를 기준으로 GuideText 분기처리
                     if #available(iOS 16.0, *) {
                         if deviceHeight < CGFloat(700.0) {
-                            StepBar_16(step: 1, frameSize: 25, fontSize: 15)
-                            GetStartGuideText(fontSize: 26)
-                        } else if deviceHeight >= CGFloat(700.0) && deviceHeight < CGFloat(750.0) {
-                            StepBar_16(step: 1, frameSize: 27, fontSize: 17)
-                            GetStartGuideText(fontSize: 22)
-                        } else if deviceHeight >= CGFloat(750.0) && deviceHeight < CGFloat(860.0) {
-                            StepBar_16(step: 1, frameSize: 28, fontSize: 18)
-                            GetStartGuideText(fontSize: 34)
+                            StepBar_16(step: 3, frameSize: 23, fontSize: 13)
+                                .padding(.top, 45)
+                            GetStartGuideText(fontSize: 23)
+                                .padding(.top, -30)
+                        } else if deviceHeight >= CGFloat(700.0) && deviceHeight < CGFloat(820.0) {
+                            StepBar_16(step: 3, frameSize: 25, fontSize: 15)
+                                .padding(.top, 45)
+                            GetStartGuideText(fontSize: 25)
+                                .padding(.top, -30)
+                        } else if deviceHeight >= CGFloat(820.0) && deviceHeight < CGFloat(860.0) {
+                            StepBar_16(step: 3, frameSize: 28, fontSize: 18)
+                                .padding(.top, 45)
+                            GetStartGuideText(fontSize: 30)
+                                .padding(.top, -30)
                         } else {
-                            StepBar_16(step: 1, frameSize: 30, fontSize: 20)
+                            StepBar_16(step: 3, frameSize: 30, fontSize: 20)
+                                .padding(.top, 45)
                             GetStartGuideText(fontSize: 34)
+                                .padding(.top, -30)
                         }
                     } else {
                         if deviceHeight < CGFloat(700.0) {
-                            StepBar_15(step: 1, frameSize: 25, fontSize: 15)
-                                .padding(.top, -20)
-                            GetStartGuideText(fontSize: 26)
-                                .padding(.top, -50)
-                        } else if deviceHeight >= CGFloat(700.0) && deviceHeight < CGFloat(750.0) {
-                            StepBar_15(step: 1, frameSize: 27, fontSize: 17)
-                            GetStartGuideText(fontSize: 27)
-                        } else if deviceHeight >= CGFloat(750.0) && deviceHeight < CGFloat(860.0) {
-                            StepBar_15(step: 1, frameSize: 28, fontSize: 18)
-                            GetStartGuideText(fontSize: 28)
+                            StepBar_15(step: 3, frameSize: 23, fontSize: 13)
+                            GetStartGuideText(fontSize: 23)
+                                .padding(.top, -30)
+                            Spacer()
+                        } else if deviceHeight >= CGFloat(700.0) && deviceHeight < CGFloat(820.0) {
+                            StepBar_15(step: 3, frameSize: 25, fontSize: 15)
+                            GetStartGuideText(fontSize: 25)
+                                .padding(.top, -30)
+                        } else if deviceHeight >= CGFloat(820.0) && deviceHeight < CGFloat(860.0) {
+                            StepBar_15(step: 3, frameSize: 28, fontSize: 18)
+                            GetStartGuideText(fontSize: 30)
+                                .padding(.top, -30)
                         } else {
-                            StepBar_15(step: 1, frameSize: 30, fontSize: 20)
+                            StepBar_15(step: 3, frameSize: 30, fontSize: 20)
                             GetStartGuideText(fontSize: 34)
+                                .padding(.top, -30)
                         }
                     }
                 }
-                .padding(.bottom, geo.size.height / 4)
+                .padding(.bottom, geo.size.height / 16)
+                //.padding(.bottom, geo.size.height / 4)
                 
                 Spacer()
                 
@@ -79,7 +102,7 @@ struct GetStartView: View {
                     }
                 } label: {
                     Text("시작하기")
-                        .font(.custom("IMHyemin-Bold", size: 16))
+                        .font(.custom("IMHyemin-Bold", size: fontSize))
                         .foregroundColor(.white)
                         .padding()
                         .frame(maxWidth: .infinity)
@@ -89,7 +112,6 @@ struct GetStartView: View {
                         }
                 }
                 .padding(.vertical, geo.size.height / 50)
-                //.padding(.vertical, 5)
             }
             .padding(.horizontal, 20)
             .navigationBarBackButtonHidden(true)

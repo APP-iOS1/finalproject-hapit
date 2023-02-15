bb3884821bc30447e77aff261525885751dde3fb
JINHYUNGP <37439745+JINHYUNGP@users.noreply.github.com>
2023-02-16 04:31:50 +0900

    [Feat] challenge 수행 시 animation 추가

--- Hapit/Hapit/View/HomeTab/ChallengeCellView.swift
+++ Hapit/Hapit/View/HomeTab/ChallengeCellView.swift
@@ -16,6 +16,7 @@ struct ChallengeCellView: View {
     @EnvironmentObject var userInfoManager: UserInfoManager
     @Environment(\.scenePhase) var scenePhase
     @State var currentUserInfos: [User]
+    @State var tempIsChecked: Bool = false
     @ObservedRealmObject var localChallenge: LocalChallenge // 로컬챌린지에서 각 필드를 업데이트 해주기 위해 선언 - 담을 그릇
     @ObservedResults(LocalChallenge.self) var localChallenges // 새로운 로컬챌린지 객체를 담아주기 위해 선언 - 데이터베이스
     var challenge: Challenge
@@ -37,104 +38,118 @@ struct ChallengeCellView: View {
     
     // MARK: - Body
     var body: some View {
-        HStack {
-            Button {
-                $localChallenge.isChecked.wrappedValue.toggle()
-            } label: {
-                Image(systemName: $localChallenge.isChecked.wrappedValue ? "checkmark.circle.fill" : "circle")
-                    .font(.title)
-                    .foregroundColor($localChallenge.isChecked.wrappedValue ? .green : Color("GrayFontColor"))
+        ZStack{
+            HStack {
+                Button {
+                    $localChallenge.isChecked.wrappedValue.toggle()
+                    
+                } label: {
+                    Image(systemName: $localChallenge.isChecked.wrappedValue ? "checkmark.circle.fill" : "circle")
+                        .font(.title)
+                        .foregroundColor($localChallenge.isChecked.wrappedValue ? .green : Color("GrayFontColor"))
+                    
+                }
+                .padding(.trailing, 5)
+                .buttonStyle(PlainButtonStyle())
                 
-            }
-            .padding(.trailing, 5)
-            .buttonStyle(PlainButtonStyle())
-            
-            //checkButton
-            VStack(alignment: .leading, spacing: 1){
-                VStack(alignment: .leading, spacing: 2){
-                    Text(challenge.createdDate)
-                        .font(.custom("IMHyemin-Regular", size: 13))
-                        .foregroundColor(Color("GrayFontColor"))
-                    Text(challenge.challengeTitle)
-                        .font(.custom("IMHyemin-Bold", size: 20))
-                        .foregroundColor(Color("MainFontColor"))
-                }//VStack
-                
-                HStack(spacing: 5){
-                    Text(Image(systemName: "flame.fill"))
-                        .foregroundColor(.orange)
-                    Text("연속 \($localChallenge.count.wrappedValue)일째")
-                        .foregroundColor(Color("MainFontColor"))
-                    Spacer()
-                    ForEach(currentUserInfos){ user in
-                        Image("\(user.proImage)")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .offset(y: 5)
-                            .frame(width: 25)
-                            .background(Color("CellColor"))
-                            .clipShape(Circle())
-                            .overlay(Circle().stroke())
+                //checkButton
+                VStack(alignment: .leading, spacing: 1){
+                    VStack(alignment: .leading, spacing: 2){
+                        Text(challenge.createdDate)
+                            .font(.custom("IMHyemin-Regular", size: 13))
                             .foregroundColor(Color("GrayFontColor"))
-                            .padding(.trailing, -12)
+                        Text(challenge.challengeTitle)
+                            .font(.custom("IMHyemin-Bold", size: 20))
+                            .foregroundColor(Color("MainFontColor"))
+                    }//VStack
+                    
+                    HStack(spacing: 5){
+                        Text(Image(systemName: "flame.fill"))
+                            .foregroundColor(.orange)
+                        Text("연속 \($localChallenge.count.wrappedValue)일째")
+                            .foregroundColor(Color("MainFontColor"))
+                        Spacer()
+                        ForEach(currentUserInfos){ user in
+                            Image("\(user.proImage)")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .offset(y: 5)
+                                .frame(width: 25)
+                                .background(Color("CellColor"))
+                                .clipShape(Circle())
+                                .overlay(Circle().stroke())
+                                .foregroundColor(Color("GrayFontColor"))
+                                .padding(.trailing, -12)
+                        }
+                    }
+                    .font(.custom("IMHyemin-Regular", size: 15))//HStack
+                    
+                }//VStack
+                Spacer()
+                
+            }//HStack
+            .padding(20)
+            //        .foregroundColor(.black)
+            .background(
+                Color("CellColor")
+            )
+            .cornerRadius(20)
+            .contentShape(.contextMenuPreview, RoundedRectangle(cornerRadius: 20))
+            .contextMenu {
+                Button(role: .destructive) {
+                    // Firestore에서 챌린지 삭제
+                    habitManager.removeChallenge(challenge: challenge)
+                    
+                    // Realm에서 챌린지 삭제
+                    $localChallenges.remove(localChallenge)
+                } label: {
+                    Text("챌린지 지우기")
+                        .font(.custom("IMHyemin-Regular", size: 17))
+                    Image(systemName: "trash")
+                }
+            } // contextMenu
+            .onAppear() {
+                if currentDate != getToday() { // 마지막에 접속한 날짜랑 현재 접속한 날짜랑 다를 경우 - 앱이 켜질 때마다 서버에 업로드되는 메모리 낭비를 방지
+                    // 로컬에 업데이트
+                    $localChallenge.count.wrappedValue = habitManager.countDays(count: $localChallenge.count.wrappedValue,
+                                                                                isChecked: $localChallenge.isChecked.wrappedValue)
+                    // 서버에 업데이트
+                    habitManager.updateCount(challenge: challenge, count: $localChallenge.count.wrappedValue)
+                    // 초기화
+                    $localChallenge.isChecked.wrappedValue = false
+                }
+                currentDate = getToday()
+                currentUserInfos = []
+                Task {
+                    // 함께 챌린지 진행하는 친구들 프사
+                    for member in challenge.mateArray {
+                        try await currentUserInfos.append(userInfoManager.getUserInfoByUID(userUid: member) ?? User(id: "", name: "", email: "", pw: "", proImage: "bearWhite", badge: [], friends: [], loginMethod: "", fcmToken: ""))
                     }
                 }
-                .font(.custom("IMHyemin-Regular", size: 15))//HStack
-                
-            }//VStack
-            Spacer()
-            
-        }//HStack
-        .padding(20)
-        //        .foregroundColor(.black)
-        .background(
-            Color("CellColor")
-        )
-        .cornerRadius(20)
-        .contentShape(.contextMenuPreview, RoundedRectangle(cornerRadius: 20))
-        .contextMenu {
-            Button(role: .destructive) {
-                // Firestore에서 챌린지 삭제
-                habitManager.removeChallenge(challenge: challenge)
-                
-                // Realm에서 챌린지 삭제
-                $localChallenges.remove(localChallenge)
-            } label: {
-                Text("챌린지 지우기")
-                    .font(.custom("IMHyemin-Regular", size: 17))
-                Image(systemName: "trash")
+         
             }
-        } // contextMenu
-        .onAppear() {
-            if currentDate != getToday() { // 마지막에 접속한 날짜랑 현재 접속한 날짜랑 다를 경우 - 앱이 켜질 때마다 서버에 업로드되는 메모리 낭비를 방지
-                // 로컬에 업데이트
-                $localChallenge.count.wrappedValue = habitManager.countDays(count: $localChallenge.count.wrappedValue,
-                                                                            isChecked: $localChallenge.isChecked.wrappedValue)
-                // 서버에 업데이트
-                habitManager.updateCount(challenge: challenge, count: $localChallenge.count.wrappedValue)
-                // 초기화
-                $localChallenge.isChecked.wrappedValue = false
-            }
-            currentDate = getToday()
-            currentUserInfos = []
-            Task {
-                // 함께 챌린지 진행하는 친구들 프사
-                for member in challenge.mateArray {
-                    try await currentUserInfos.append(userInfoManager.getUserInfoByUID(userUid: member) ?? User(id: "", name: "", email: "", pw: "", proImage: "bearWhite", badge: [], friends: [], loginMethod: "", fcmToken: ""))
+            .onChange(of: scenePhase) { _ in // 마지막에 접속한 날짜랑 현재 접속한 날짜랑 다를 경우
+                if currentDate != getToday() { // 자정이 되는 순간
+                    // 로컬에 업데이트
+                    $localChallenge.count.wrappedValue = habitManager.countDays(count: $localChallenge.count.wrappedValue,
+                                                                                isChecked: $localChallenge.isChecked.wrappedValue)
+                    // 서버에 업데이트
+                    habitManager.updateCount(challenge: challenge, count: $localChallenge.count.wrappedValue)
+                    // 초기화
+                    $localChallenge.isChecked.wrappedValue = false
                 }
+                currentDate = getToday()
             }
-        }
-        .onChange(of: scenePhase) { _ in // 마지막에 접속한 날짜랑 현재 접속한 날짜랑 다를 경우
-            if currentDate != getToday() { // 자정이 되는 순간
-                // 로컬에 업데이트
-                $localChallenge.count.wrappedValue = habitManager.countDays(count: $localChallenge.count.wrappedValue,
-                                                                            isChecked: $localChallenge.isChecked.wrappedValue)
-                // 서버에 업데이트
-                habitManager.updateCount(challenge: challenge, count: $localChallenge.count.wrappedValue)
-                // 초기화
-                $localChallenge.isChecked.wrappedValue = false
+            
+            if $localChallenge.isChecked.wrappedValue == true {
+                
+                    JellyConfetti(title: "")
+//                        .onAppear{
+//                            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//                                isShownConfetti = false
+//                            }
+//                        }
             }
-            currentDate = getToday()
         }
     }
 }
